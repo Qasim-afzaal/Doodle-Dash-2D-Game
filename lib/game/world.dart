191b5df63c0007122e9982cfc7f825c0b8435ead
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-02-04 04:24:19 +0500

    code refatcor

--- lib/game/world.dart
+++ lib/game/world.dart
@@ -3,11 +3,21 @@ import 'package:flame/parallax.dart';
 
 import './doodle_dash.dart';
 
-class World extends ParallaxComponent<DoodleDash> {
+class World extends ParallaxComponent {
+  final DoodleDash game;
+
+  World(this.game);
+
   @override
   Future<void> onLoad() async {
-    parallax = await gameRef.loadParallax(
-      [ParallaxImageData('game/graph_paper.png')],
+    super.onLoad();
+    parallax = await game.loadParallax(
+      [
+        ParallaxImageData('game/graph_paper.png'),
+        ParallaxImageData('game/clouds.png'), // Added background layer
+      ],
+      baseVelocity: Vector2(20, 0), // Adding parallax scrolling effect
+      velocityMultiplierDelta: Vector2(1.5, 0), // Depth effect
     );
   }
 }
